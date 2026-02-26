7e7593b92f65ef8f1e0400f6035ed0783a41bde2
Marco Mascheroni <marco.mascheroni@cern.ch>
2026-02-26 14:37:21 +0100

    Add some comments to run.sh

--- samples/htcondor/run.sh
+++ samples/htcondor/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Run stepchain in background so we can monitor it with prmon
 ./execute_stepchain.sh request_psets.tar.gz job*.json &
 PID=$!
-prmon --pid $PID --filename prmon.txt --json-summary prmon.json
+
+# Profile memory usage; || true so prmon failure (e.g. not found) does not abort the job
+prmon --pid $PID --filename prmon.txt --json-summary prmon.json || true
+
+# Wait for stepchain and exit with its status (wait returns immediately if process already exited)
+wait $PID
+exit $?
